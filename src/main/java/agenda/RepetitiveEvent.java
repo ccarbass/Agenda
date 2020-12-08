42d9113b56c8a4a965f6ab1fa6b41acfafeb8a60
BuildTools <unconfigured@null.spigotmc.org>
2020-12-08 17:09:16 +0100

    implémentation repetitive event non fini

--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -1,31 +1,39 @@
-
 package agenda;
 
-import java.util.*;
-import java.time.*;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
 /**
  * Description : A repetitive Event
  */
 public class RepetitiveEvent extends Event {
+
+    public Duration duration;
+    public ChronoUnit frequency;
+    public ArrayList<LocalDate> exceptions;
+
     /**
      * Constructs a repetitive event
      *
-     * @param title the title of this event
-     * @param start the start of this event
-     * @param duration myDuration in seconds
+     * @param title     the title of this event
+     * @param start     the start of this event
+     * @param duration  myDuration in seconds
      * @param frequency one of :
-     * <UL>
-     * <LI>ChronoUnit.DAYS for daily repetitions</LI>
-     * <LI>ChronoUnit.WEEKS for weekly repetitions</LI>
-     * <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
-     * </UL>
+     *                  <UL>
+     *                  <LI>ChronoUnit.DAYS for daily repetitions</LI>
+     *                  <LI>ChronoUnit.WEEKS for weekly repetitions</LI>
+     *                  <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
+     *                  </UL>
      */
     public RepetitiveEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency) {
         super(title, start, duration);
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+
+        this.frequency = frequency;
+        this.duration = duration;
+        this.exceptions = new ArrayList();
     }
 
     /**
@@ -34,17 +42,14 @@ public class RepetitiveEvent extends Event {
      * @param date the event will not occur at this date
      */
     public void addException(LocalDate date) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+       this.exceptions.add(date);
     }
 
     /**
-     *
      * @return the type of repetition
      */
     public ChronoUnit getFrequency() {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");    
+        return this.frequency;
     }
 
 }
