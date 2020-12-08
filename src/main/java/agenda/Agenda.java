2a5333aa77be65c09a8472586edf6713e64c5b3b
BuildTools <unconfigured@null.spigotmc.org>
2020-12-08 17:22:25 +0100

    implémentation agenda inDay

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -1,7 +1,10 @@
 package agenda;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Description : An agenda that stores events
@@ -9,14 +12,15 @@ import java.util.*;
 public class Agenda {
 
     public Set<Event> events = new HashSet<>();
+
     /**
      * Adds an event to this agenda
      *
      * @param e the event to add
      */
     public void addEvent(Event e) {
-        for(Event event : events){
-            if(event.equals(e)){
+        for (Event event : events) {
+            if (event.equals(e)) {
                 events.remove(e);
             }
         }
@@ -30,7 +34,16 @@ public class Agenda {
      * @return list and iteraror to the events that occur on that day
      */
     public List<Event> eventsInDay(LocalDate day) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        ArrayList<Event> today = new ArrayList();
+        for (Event e : events) {
+
+            if (e.isInDay(day)) {
+
+                today.add(e);
+
+            }
+
+        }
+        return today;
     }
 }
