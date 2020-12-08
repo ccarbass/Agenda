b65dd56f9695b16df385b4cbf15b1568948b63d2
Camille <ca.carbasse@orange.fr>
2020-12-08 12:25:02 +0100

    debut de l'implementation

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -7,21 +7,27 @@ import java.util.*;
  * Description : An agenda that stores events
  */
 public class Agenda {
+
+    public Set<Event> events = new HashSet<>();
     /**
      * Adds an event to this agenda
      *
      * @param e the event to add
      */
     public void addEvent(Event e) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        for(Event event : events){
+            if(event.equals(e)){
+                events.remove(e);
+            }
+        }
+        events.add(e);
     }
 
     /**
      * Computes the events that occur on a given day
      *
      * @param day the day toi test
-     * @return and iteraror to the events that occur on that day
+     * @return list and iteraror to the events that occur on that day
      */
     public List<Event> eventsInDay(LocalDate day) {
         // TODO : implémenter cette méthode
